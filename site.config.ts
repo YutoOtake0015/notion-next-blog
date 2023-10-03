b788c4a3741a0df13c67e894e9eb1c521ec83aba
OtakeYuto <yuto.programming@gmail.com>
2023-10-03 20:23:44 +0900

    first commit

---
+++ site.config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  title: "Notion Next Blog",
+  twitterUrl: "https://twitter.com/yu__programming",
+  instagramUrl: "",
+  gitHubUrl: "https://github.com/YutoOtake0015",
+};
+ 
---
+++ utils/samplePAgesObject.ts
@@ -0,0 +1,132 @@
+export const sample = [
+  {
+    object: "page",
+    id: "1dbc835d-7d80-4447-8eb0-61892fe53f48",
+    created_time: "2023-10-01T07:40:00.000Z",
+    last_edited_time: "2023-10-01T08:02:00.000Z",
+    created_by: {
+      object: "user",
+      id: "af1e9fc8-8b23-4bf9-9bba-2773932c7b25",
+    },
+    last_edited_by: {
+      object: "user",
+      id: "af1e9fc8-8b23-4bf9-9bba-2773932c7b25",
+    },
+    cover: {
+      type: "external",
+      external: {
+        url: "https://www.notion.so/images/page-cover/woodcuts_1.jpg",
+      },
+    },
+    icon: null,
+    parent: {
+      type: "database_id",
+      database_id: "dd831419-3dd6-4c43-bddd-f0bf86597df3",
+    },
+    archived: false,
+    properties: {
+      tags: {
+        id: "HkjP",
+        type: "multi_select",
+        multi_select: [
+          {
+            id: "248c6bd0-1fba-4dae-8e57-5b818918f8d5",
+            name: "japan",
+            color: "default",
+          },
+        ],
+      },
+      published: {
+        id: "Mqgh",
+        type: "date",
+        date: {
+          start: "2023-10-01",
+          end: null,
+          time_zone: null,
+        },
+      },
+      author: {
+        id: "R_Xb",
+        type: "rich_text",
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: "Yuto Otake",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "Yuto Otake",
+            href: null,
+          },
+        ],
+      },
+      slug: {
+        id: "nvk%5D",
+        type: "rich_text",
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: "japan",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "japan",
+            href: null,
+          },
+        ],
+      },
+      "Created time": {
+        id: "%7BiwZ",
+        type: "created_time",
+        created_time: "2023-10-01T07:40:00.000Z",
+      },
+      isPublic: {
+        id: "~uei",
+        type: "checkbox",
+        checkbox: true,
+      },
+      name: {
+        id: "title",
+        type: "title",
+        title: [
+          {
+            type: "text",
+            text: {
+              content: "Japan",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "Japan",
+            href: null,
+          },
+        ],
+      },
+    },
+    url: "https://www.notion.so/Japan-1dbc835d7d8044478eb061892fe53f48",
+    public_url:
+      "https://righteous-frown-7b2.notion.site/Japan-1dbc835d7d8044478eb061892fe53f48",
+  },
+];
